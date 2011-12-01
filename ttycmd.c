668b935fec9aa600a4915b4df123e61c7f1b524d
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-11-30 21:43:40 -0500

    got bluetooth and comm working

--- ttycmd.c
+++ ttycmd.c
@@ -6,18 +6,53 @@
 #include <termios.h>
 #include <pthread.h>
 
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/l2cap.h>
+
+#include "cv.h"
+#include "highgui.h"
+
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 
 static int tty_fd;
 static pthread_t cmd_thread;
 static pthread_t comm_thread;
+static pthread_t intel_thread;
+static pthread_t bt_thread;
+static pthread_t camera_thread;
 static void* cmd_thread_status;
 static void* comm_thread_status;
+static void* intel_thread_status;
+static void* bt_thread_status;
+static void* camera_thread_status;
 
 char default_tty_dev[] = "/dev/ttyACM0";
 
 #define NELEMENTS(_array)	(sizeof(_array) / sizeof(_array[0]))
+
+// Camera stuff
+#define DEBUGMODE
+#define QUALIFY_THRESHOLD (83)
+#define VICTORY_THRESHOLD (95)
+#define DIRECTION_THRESHOLD (59) 
+
+/* these are mainly  for bluetooth output */
+int GLOBAL_SPEED = 0; 
+int GLOBAL_MODE = 0; 
+int GLOBAL_SENSOR_RIGHT = 0;
+int GLOBAL_SENSOR_LEFT = 0; 
+int GLOBAL_SENSOR_CENTER = 0; 
+
+/*
+	-2 is default (consider making teensy wait) 
+	-1 is victory
+	1 is left
+	2 is middle
+	3 is right
+*/
+int GLOBAL_WANTED_DIRECTION = 0; 
 
 #define CMD_TEENSY_MODE		(0x80 | 0x01)
 #define CMD_CHANGE_STATE	(0x80 | 0x02)
@@ -180,12 +215,12 @@ uint8 get_command_id(char* cmd_name)
 
 uint8 get_decimal_value(char* decimal_str)
 {
-	int decimal;
+	uint8 decimal;
 
 	if (decimal_str == NULL)
 		return 0;
 
-	decimal = atoi(decimal_str);
+	decimal = (uint8) atoi(decimal_str);
 
 	return decimal;
 }
@@ -197,6 +232,346 @@ void send_command(int fd, uint8 cmd, uint8 val)
 
 	write(fd, &cmd, 1);
 	write(fd, &val, 1);
+}
+
+void* IntelThreadProc(void* data)
+{
+	send_command(tty_fd, CMD_CHANGE_STATE, STATE_ORDERS);
+
+	while(1)
+	{
+		if(-1 == GLOBAL_WANTED_DIRECTION) 
+		{
+			// VICTORY DANCE
+			send_command(tty_fd, CMD_CHANGE_STATE, STATE_DANCE);
+			sleep(10);
+			send_command(tty_fd, CMD_CHANGE_STATE, STATE_ORDERS);
+		}
+		else if (35 > GLOBAL_SENSOR_CENTER)
+		{
+			// Do crazy backup
+			printf("do crazy backup\n");
+			send_command(tty_fd, CMD_SPEED, 0);
+			sleep(1);
+			send_command(tty_fd, CMD_SPEED, 127);
+			send_command(tty_fd, CMD_HARD_TURN, TURN_LEFT);
+			sleep(1);
+			send_command(tty_fd, CMD_HARD_TURN, TURN_NONE);
+ 		} 
+		else if ((55 > GLOBAL_SENSOR_LEFT) || (3 == GLOBAL_WANTED_DIRECTION))
+		{
+			// SOFT TURN RIGHT
+			send_command(tty_fd, CMD_SOFT_TURN, TURN_RIGHT);
+		}
+		else if ((55 > GLOBAL_SENSOR_RIGHT) || (1 == GLOBAL_WANTED_DIRECTION))
+		{
+			// SOFT TURN LEFT
+			send_command(tty_fd, CMD_SOFT_TURN, TURN_LEFT);
+		}
+		else
+		{
+			send_command(tty_fd, CMD_HARD_TURN, TURN_NONE);
+			send_command(tty_fd, CMD_SPEED, 127);
+			send_command(tty_fd, CMD_SET_DIRECTION, MOVE_FORWARD);
+		}
+
+		sleep(1);
+	}
+}
+
+void* BTThreadProc(void* data)
+{
+  struct sockaddr_l2 addr = { 0 };
+  int s, status;
+  char buffer[100];
+  char dest[18] = "00:02:72:16:1A:C1"; /* This is the address of the dongle on my laptop */
+  char sendbuffer[100] = { 0 }; 
+
+  while(1)
+  {
+    sleep(1);
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+
+    // set the connection parameters (who to connect to)
+    addr.l2_family = AF_BLUETOOTH;
+    addr.l2_psm = htobs(0x1001);
+    str2ba( dest, &addr.l2_bdaddr );
+
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    // send a message
+    if (status == 0) 
+    {
+        memset(sendbuffer, 0, sizeof(sendbuffer));
+        strcpy(sendbuffer, "mode: "); 
+        sprintf(buffer, "%i", GLOBAL_MODE); 
+        strcat(sendbuffer, buffer); 
+        strcat(sendbuffer, ", ");
+
+        strcat(sendbuffer, get_state_name(GLOBAL_MODE));
+
+        strcat(sendbuffer, "\n"); 
+
+        strcat(sendbuffer, "speed: "); 
+        sprintf(buffer, "%i", GLOBAL_SPEED);
+        strcat(sendbuffer, buffer); 
+        strcat(sendbuffer, "\n"); 
+
+                    strcat(sendbuffer, "distance.right: "); 
+        if (GLOBAL_SENSOR_RIGHT < 0xFF)  
+        {
+          sprintf(buffer, "%i", GLOBAL_SENSOR_RIGHT);
+          strcat(sendbuffer, buffer); 
+        }
+        else 
+          strcat(sendbuffer, "Far, far, away..."); 
+        
+        strcat(sendbuffer, "\n"); 
+ 
+                    strcat(sendbuffer, "distance.center: "); 
+        if (GLOBAL_SENSOR_CENTER < 0xFF)  
+        {
+          sprintf(buffer, "%i", GLOBAL_SENSOR_CENTER);
+          strcat(sendbuffer, buffer); 
+        }
+        else 
+          strcat(sendbuffer, "Far, far, away..."); 
+        strcat(sendbuffer, "\n");                    
+
+                    strcat(sendbuffer, "distance.right: "); 
+        if (GLOBAL_SENSOR_LEFT < 0xFF)  
+        {
+          sprintf(buffer, "%i", GLOBAL_SENSOR_LEFT);
+          strcat(sendbuffer, buffer); 
+        }
+        else 
+          strcat(sendbuffer, "Far, far, away..."); 
+
+        strcat(sendbuffer, "\n"); 
+        status = write(s, sendbuffer, sizeof(sendbuffer)); 
+    }
+
+    if( status < 0 ) perror("DERP!");
+  }
+}
+
+/* opencv stuff goes here */
+void* CameraThreadProc(void* tdata)
+{
+    CvCapture *capture = 0;
+    IplImage *frame = 0;
+    int key = 0;
+    int loop; 
+    
+    int arg_index; 
+
+    /* Mainly for loops */
+    int i;
+    int j;
+    int k;
+    unsigned char screen_section; 
+    unsigned int screen_segment; 
+
+    int step; 
+    int height;
+    int width; 
+    int channels;
+    
+    unsigned long count_red; 
+
+    /* 
+      Depending on these percentages, and the defined threshold,
+      we will know and tell the vehicle appropriately which way to
+      go. 
+    */
+    unsigned int total_section1 = 0;
+    unsigned int total_section2 = 0;
+    unsigned int total_section3 = 0;
+    double percent_r_section1 = 0.0f; 
+    double percent_r_section2 = 0.0f; 
+    double percent_r_section3 = 0.0f; 
+    char direction[10];
+    
+    /* Controls what color we're looking for */
+    unsigned char channel_checking = 0; 
+
+    unsigned char *data; 
+ 
+    channel_checking = 2; // red
+
+#if 0
+    for (arg_index=0; arg_index<argc; ++arg_index)
+    {
+      if (!strcmp(argv[arg_index],"--color=blue"))
+      {
+        channel_checking = 0; 
+        printf("Detecting blue\n");
+      }
+      if (!strcmp(argv[arg_index],"--color=green")) 
+      {
+        channel_checking = 1; 
+        printf("Detecting green\n");
+      }
+      if (!strcmp(argv[arg_index],"--color=red")) 
+      {
+        channel_checking = 2; 
+        printf("Detecting red\n");
+      }
+    }
+#endif 
+
+    /* initialize camera */
+    capture = cvCaptureFromCAM( 0 );
+ 
+    /* always check */
+    if ( !capture ) {
+        fprintf( stderr, "Cannot open initialize webcam!\n" );
+        return 1;
+    }
+ 
+    /* create a window for the video */
+    // cvNamedWindow( "result", CV_WINDOW_AUTOSIZE );
+ 
+    while( key != 'q' ) {
+        /* get a frame */
+        frame = cvQueryFrame( capture );
+
+        /* always check */
+        if( !frame ) break;
+       
+        data = (unsigned char*) frame->imageData; 
+
+        height = frame->height; 
+        width = frame->width; 
+        step = frame->widthStep;
+        channels = frame->nChannels;
+
+        screen_segment = width / 3; 
+
+        count_red = 0 ;
+        total_section1 = 0;       
+        total_section2 = 0;       
+        total_section3 = 0;       
+        
+        for(i=0;i<height;i++) 
+        {
+          for(j=0;j<width;j++) 
+          {
+            
+            /* Check if white */
+            if (data[i*step+j*channels+2] >= QUALIFY_THRESHOLD &&
+                data[i*step+j*channels+0] >= QUALIFY_THRESHOLD && 
+                data[i*step+j*channels+1] >= QUALIFY_THRESHOLD   ) 
+            {
+              ++count_red;
+
+              /* Right Segment */
+              if (j>=screen_segment*2)
+              {
+                ++total_section3;
+              }
+              /* Middle Segment */
+              else if (j>=screen_segment)
+              {
+                ++total_section2; 
+              }
+              /* Left Segment */
+              else 
+              {
+                ++total_section1; 
+              }
+  
+            }
+
+          } 
+        }
+
+        // printf("countred: %d\n", count_red);
+
+        percent_r_section1 = 100 * (double) total_section1 / (screen_segment * height); 
+        percent_r_section2 = 100 * (double) total_section2 / (screen_segment * height); 
+        percent_r_section3 = 100 * (double) total_section3 / (screen_segment * height); 
+
+#ifdef DEBUGMODE
+
+        /*
+            Note - This is your intelligence!
+            Watch it crumble! When adding to the
+            beagle board, you should just set
+            the global flag here so that the thread
+            edits said variable. 
+        */
+
+        /* left case */ 
+        if( percent_r_section1 >= VICTORY_THRESHOLD )
+        {
+          strcpy(direction,"[victory]");      
+          GLOBAL_WANTED_DIRECTION = -1; 
+        }
+        else if 
+        ( percent_r_section3 > percent_r_section2 && 
+          percent_r_section3 > percent_r_section1 && 
+          percent_r_section3 >= DIRECTION_THRESHOLD) 
+        {
+          strcpy(direction,"[left]");      
+          GLOBAL_WANTED_DIRECTION = 1; 
+        }
+
+        /* right case */
+        else if 
+        ( percent_r_section1 > percent_r_section2 && 
+          percent_r_section1 > percent_r_section3 && 
+          percent_r_section1 >= DIRECTION_THRESHOLD)
+        {
+          strcpy(direction,"[right]");      
+          GLOBAL_WANTED_DIRECTION = 1; 
+        }
+
+        /* forward case */
+        else if 
+        ( percent_r_section2 > percent_r_section1 &&
+          percent_r_section2 > percent_r_section3 && 
+          percent_r_section2 >= DIRECTION_THRESHOLD)
+        {
+          strcpy(direction,"[forward]");      
+          GLOBAL_WANTED_DIRECTION = 2; 
+        }
+
+        /* default */
+        else 
+        {
+          strcpy(direction,"[default]");      
+	  GLOBAL_WANTED_DIRECTION = -2; 
+        }
+
+        /*printf
+        (
+          "x:%d,y:%d,[r%%:%f][r%%:%f][r%%:%f] : I want to go... %s    ", 
+          frame->width
+          ,frame->height
+          ,percent_r_section1 
+          ,percent_r_section2 
+          ,percent_r_section3 
+          ,direction
+        );*/
+
+        for(loop=0; loop<1000; ++loop)
+          printf("\b"); 
+
+#endif 
+        /* display current frame - have disabled when using beagle */
+        // cvShowImage( "result", frame );
+ 
+        /* exit if user press 'q' */
+        key = cvWaitKey( 1 );
+    }
+ 
+    /* free memory */
+    // cvDestroyWindow( "result" );
+    cvReleaseCapture( &capture );
+ 
 }
 
 void* CmdThreadProc(void* data)
@@ -354,11 +729,35 @@ void* CommThreadProc(void* data)
 {
         uint8 comm = 0;
 
-        while (0)
+        while (1)
         {
                 if (read(tty_fd, &comm, 1) > 0)
                 {
-                        printf("0x%02X\n", comm);
+                        // printf("0x%02X\n", comm);
+
+			switch (comm)
+			{
+				case CMD_DIST_LEFT:
+					read(tty_fd, &comm, 1);
+					GLOBAL_SENSOR_LEFT = comm;
+					break;
+
+				case CMD_DIST_RIGHT:
+					read(tty_fd, &comm, 1);
+					GLOBAL_SENSOR_RIGHT = comm;
+					break;
+
+				case CMD_DIST_CENTER:
+					read(tty_fd, &comm, 1);
+					GLOBAL_SENSOR_CENTER = comm;
+					break;
+
+				case CMD_TEENSY_MODE:
+					read(tty_fd, &comm, 1);
+					GLOBAL_MODE = comm;
+					break;
+
+			}
                 }
         }
 
@@ -399,9 +798,15 @@ int main(int argc, char** argv)
 
 	pthread_create(&cmd_thread, NULL, CmdThreadProc, NULL);
 	pthread_create(&comm_thread, NULL, CommThreadProc, NULL);
+	pthread_create(&intel_thread, NULL, IntelThreadProc, NULL);
+	pthread_create(&bt_thread, NULL, BTThreadProc, NULL);
+	pthread_create(&camera_thread, NULL, CameraThreadProc, NULL);
 
 	pthread_join(cmd_thread, cmd_thread_status);
 	pthread_join(comm_thread, &comm_thread_status);
+	pthread_join(intel_thread, &intel_thread_status);
+	pthread_join(bt_thread, &bt_thread_status);
+	pthread_join(camera_thread, &camera_thread_status);
 
 	close(tty_fd);
 
